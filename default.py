c3ab7a2fff0067b4adb54f8e11a6a062b5e0899a
Nicole Ng <33305075+nmng98@users.noreply.github.com>
2019-06-23 23:36:26 -0700

    displays pages

--- default.py
+++ default.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-# -------------------------------------------------------------------------
-# This is a sample controller
-# this file is released under public domain and you can use without limitations
-# -------------------------------------------------------------------------
 
-# ---- example index page ----
 def index():
     return dict()
 def movie():
